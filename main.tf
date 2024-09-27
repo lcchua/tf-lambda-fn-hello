7860b845a09ea13ca0e8b20ed66f1d25a64a023f
lcchua <laichwang@gmail.com>
2024-09-27 15:58:08 +0800

    Corrected the role attribute to name value in the iam role-policy attachment resource block

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_iam_policy" "lambdafn_iam_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-    role        = aws_iam_role.lambdafn_iam_role
+    role        = aws_iam_role.lambdafn_iam_role.name
     policy_arn  = aws_iam_policy.lambdafn_iam_policy.arn
 }
 
