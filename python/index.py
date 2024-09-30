3f14713874ca756b1c53d76805cc0ada18b7f2a2
lcchua <laichwang@gmail.com>
2024-09-30 10:15:02 +0800

    Sync Python pgm to  remote and local repos

--- python/index.py
+++ python/index.py
@@ -1,9 +1,43 @@
 
+import json
+
 def lambda_handler(event, context):
+    try:
+        # If the event is wrapped in a "body" field, parse it
+        if 'body' in event:
+            event = json.loads(event['body'])
+        
+        print(event)
+        message = 'Hello {} !'.format(event.get('key1'))
+        
+        # Return the proper format
+        return {
+            'statusCode': 200,  # HTTP status code
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': message
+            })  # Body needs to be a JSON string
+        }
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
+
+""" def lambda_handler(event, context):
    message = 'Hello {} !'.format(event['key1'])
    return {
        'message' : message
-   }
+   } """
 
 
 # Example 1: Alternative Python pgm code for test, uncomment as needed
